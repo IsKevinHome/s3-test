fff9c4b76a95baccdd6b79dd7992acf1e719dc94
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2026-02-16 19:14:05 -0800

    feat: add db plugin

---
+++ src/plugins/database.js
@@ -0,0 +1,10 @@
+import { connectDatabase, disconnectDatabase } from '../config/database.js';
+
+export default async function databasePlugin(fastify, options) {
+  await connectDatabase();
+
+  // this hook fires when when the server shuts down.
+  fastify.addHook('onClose', async (instance) => {
+    await disconnectDatabase();
+  });
+}
