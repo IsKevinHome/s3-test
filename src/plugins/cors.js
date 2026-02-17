f30256cf1550613c177b9c42696eec2e7546f77a
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2026-02-16 19:24:16 -0800

    feat: add cors plugin

---
+++ src/plugins/cors.js
@@ -0,0 +1,12 @@
+import cors from '@fastify/cors';
+
+const VITE_DEFAULT_ORIGIN = 'http://localhost:5173';
+
+export default async function corsPlugin(fastify, options) {
+  await fastify.register(cors, {
+    origin: process.env.CORS_ORIGIN || VITE_DEFAULT_ORIGIN,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+}
